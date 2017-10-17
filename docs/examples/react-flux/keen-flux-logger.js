0007d25414c4a2b7f3686260e3f9da8c6c63faac
dustinlarimer <dustinlarimer@gmail.com>
2017-10-17 13:51:49 -0700

    Use auto-tracking data modeling for React examples

--- docs/examples/react-flux/keen-flux-logger.js
+++ docs/examples/react-flux/keen-flux-logger.js
@@ -19,78 +19,14 @@ if (process.env.NODE_ENV !== 'production') {
   // Optionally prevent recording in dev mode
   Keen.enabled = false;
   // Display events in the browser console
-  client.on('recordEvent', KeenTracking.log);
+  client.on('recordEvent', Keen.log);
 }
 
-const helpers = Keen.helpers;
-const timer = Keen.utils.timer();
-timer.start();
-
-// Batch-record events every 5s
-client.queueInterval(5);
-
-// Define a baseline data model for every
-// action/event that will be recorded
-client.extendEvents(() => {
-  return {
-    geo: {
-      info: { /* Enriched */ },
-      ip_address: '${keen.ip}',
-    },
-    page: {
-      info: { /* Enriched */ },
-      title: document.title,
-      url: document.location.href
-    },
-    referrer: {
-      info: { /* Enriched */ },
-      url: document.referrer
-    },
-    tech: {
-      browser: helers.getBrowserProfile(),
-      info: { /* Enriched */ },
-      user_agent: '${keen.user_agent}'
-    },
-    time: helpers.getDatetimeIndex(),
-    visitor: {
-      time_on_page: timer.value()
-      /* Include additional visitor info here */
-    },
-    keen: {
-      timestamp: new Date().toISOString(),
-      addons: [
-        {
-          name: 'keen:ip_to_geo',
-          input: {
-            ip: 'geo.ip_address'
-          },
-          output : 'geo.info'
-        },
-        {
-          name: 'keen:ua_parser',
-          input: {
-            ua_string: 'tech.user_agent'
-          },
-          output: 'tech.info'
-        },
-        {
-          name: 'keen:url_parser',
-          input: {
-            url: 'page.url'
-          },
-          output: 'page.info'
-        },
-        {
-          name: 'keen:referrer_parser',
-          input: {
-            referrer_url: 'referrer.url',
-            page_url: 'page.url'
-          },
-          output: 'referrer.info'
-        }
-      ]
-    }
-  };
+// Track a 'pageview' event and initialize auto-tracking data model
+client.initAutoTracking({
+  recordClicks: false,
+  recordFormSubmits: false,
+  recordPageViews: true
 });
 
 const fluxLogger = function(state, action) {
@@ -106,7 +42,7 @@ const fluxLogger = function(state, action) {
   // Filter omitted actions by action.type
   // ...or whatever you name this property
   if (OMITTED_ACTIONS.indexOf(action.type) < 0) {
-    client.deferEvent(EVENT_STREAM_NAME, eventBody);
+    client.recordEvent(EVENT_STREAM_NAME, eventBody);
   }
 }
 
--- docs/examples/react-redux-middleware/keen-redux-middleware.js
+++ docs/examples/react-redux-middleware/keen-redux-middleware.js
@@ -10,8 +10,8 @@ const OMITTED_ACTIONS = [
 
 // Define a client instance
 const client = new Keen({
-  projectId: 'PROJECT_ID',
-  writeKey: 'WRITE_KEY'
+  projectId: 'YOUR_PROJECT_ID',
+  writeKey: 'YOUR_WRITE_KEY'
 });
 
 if (process.env.NODE_ENV !== 'production') {
@@ -21,74 +21,11 @@ if (process.env.NODE_ENV !== 'production') {
   client.on('recordEvent', Keen.log);
 }
 
-const helpers = Keen.helpers;
-const timer = Keen.utils.timer();
-timer.start();
-
-// Batch-record events every 5s
-client.queueInterval(5);
-
-// Define a baseline data model for every
-// action/event that will be recorded
-client.extendEvents(() => {
-  return {
-    geo: {
-      info: { /* Enriched */ },
-      ip_address: '${keen.ip}',
-    },
-    page: {
-      info: { /* Enriched */ },
-      title: document.title,
-      url: document.location.href
-    },
-    referrer: {
-      info: { /* Enriched */ },
-      url: document.referrer
-    },
-    tech: {
-      browser: helers.getBrowserProfile(),
-      info: { /* Enriched */ },
-      user_agent: '${keen.user_agent}'
-    },
-    time: helpers.getDatetimeIndex(),
-    visitor: {
-      time_on_page: timer.value()
-      /* Include additional visitor info here */
-    },
-    keen: {
-      addons: [
-        {
-          name: 'keen:ip_to_geo',
-          input: {
-            ip: 'geo.ip_address'
-          },
-          output : 'geo.info'
-        },
-        {
-          name: 'keen:ua_parser',
-          input: {
-            ua_string: 'tech.user_agent'
-          },
-          output: 'tech.info'
-        },
-        {
-          name: 'keen:url_parser',
-          input: {
-            url: 'page.url'
-          },
-          output: 'page.info'
-        },
-        {
-          name: 'keen:referrer_parser',
-          input: {
-            referrer_url: 'referrer.url',
-            page_url: 'page.url'
-          },
-          output: 'referrer.info'
-        }
-      ]
-    }
-  };
+// Track a 'pageview' event and initialize auto-tracking data model
+client.initAutoTracking({
+  recordClicks: false,
+  recordFormSubmits: false,
+  recordPageViews: true
 });
 
 const reduxMiddleware = function({ getState }) {
@@ -106,7 +43,7 @@ const reduxMiddleware = function({ getState }) {
     // Filter omitted actions by action.type
     // ...or whatever you name this property
     if (OMITTED_ACTIONS.indexOf(action.type) < 0) {
-      client.deferEvent(EVENT_STREAM_NAME, eventBody);
+      client.recordEvent(EVENT_STREAM_NAME, eventBody);
     }
     return returnValue;
   };
