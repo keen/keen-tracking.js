e568269ca813baf3210f02fc1694989b883247d6
TunedMidja <tunedmidja@gmail.com>
2017-10-19 21:32:32 +0200

    Nicer looking UI with @angular/material.

--- docs/examples/angular/keen-angular-example/src/app/app.module.ts
+++ docs/examples/angular/keen-angular-example/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { KeenService } from './keen.service';
 import { FirstPageComponent } from './first-page/first-page.component';
@@ -17,7 +19,13 @@ import { appRoutes } from './app.routes';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)    
   ],
   providers: [KeenService],
   bootstrap: [AppComponent],
