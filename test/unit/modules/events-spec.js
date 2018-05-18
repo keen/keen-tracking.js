6d90f62c47cfcb0b789bb6eb49a8790963c0a1d8
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-05-18 16:10:11 +0200

    fix test for events

--- test/unit/modules/events-spec.js
+++ test/unit/modules/events-spec.js
@@ -1,109 +1,77 @@
-
-import Keen from '../../../../lib/server';
+import Keen from '../../../lib/server';
 
 describe('Keen event emitter system', () => {
+  const mockFn1 = jest.fn();
+  const mockFn2 = jest.fn();
 
   beforeEach(() => {
     // Clear out events from previous tests
     Keen.off();
+    mockFn1.mockReset();
+    mockFn2.mockReset();
   });
 
   describe('#on', () => {
     it('should attach custom event listeners with #on', () => {
       Keen.on('event', () => {});
-      expect().isArray(Keen.listeners());
+      expect(Keen.listeners()).toBeInstanceOf(Array);
     });
   });
 
   describe('#trigger', () => {
-    it('should call bound functions when triggered', function(done){
-      const count = 0;
-      Keen.on('event', () => {
-        count++;
-        if (count === 1) {
-          done();
-        }
-        else {
-          throw Error('Called incorrectly');
-        }
-      });
+    it('should call bound functions when triggered', () => {
+      Keen.on('event', mockFn1);
       Keen.emit('event');
+      expect(mockFn1).toBeCalled();
     });
 
-    it('should pass arguments to bound functions when triggered', function(done){
-      const payload = { status: 'ok' }, count = 0;
-      Keen.on('event', function(data){
-        count++;
-        if (count === 1 && data.status === 'ok') {
-          done();
-        }
-        else {
-          throw Error('Called incorrectly');
-        }
-      });
+    it('should pass arguments to bound functions when triggered', () => {
+      const payload = { status: 'ok' };
+      Keen.on('event', mockFn1);
       Keen.emit('event', payload);
+      expect(mockFn1).toBeCalledWith(payload);
     });
 
     it('should call bound functions multiple when triggered multiple times', () => {
       // const callback = chai.spy();
-      const count = 0;
-      Keen.on('event', () => {
-        count++;
-      });
+      Keen.on('event', mockFn1);
       Keen.emit('event');
       Keen.emit('event');
       Keen.emit('event');
-      expect().equal(count, 3);
-      // expect(callback).to.have.been.called.exactly(3);
+      expect(mockFn1.mock.calls.length).toBe(3);
     });
   });
 
   describe('#off', () => {
     it('should remove all listeners for an event name with #off(name)', () => {
-      const count = 0;
-      function callback(){
-        count++;
-      };
-      Keen.on('event', callback);
-      Keen.on('event', callback);
+      Keen.on('event', mockFn1);
+      Keen.on('event', mockFn1);
       Keen.off('event');
       Keen.emit('event');
-      expect().equal(count, 0);
+      expect(mockFn1.mock.calls.length).toBe(0);
     });
 
     it('should remove specified listeners with #off(name, callback)', () => {
-      const count = 0;
-      function callback(){
-        count++;
-      }
-      function fakeback(){
-        throw Error('Don\'t call me!');
-      };
-      Keen.on('event', callback);
-      Keen.on('event', fakeback);
-      Keen.off('event', fakeback);
+      Keen.on('event', mockFn1);
+      Keen.on('event', mockFn2);
+      Keen.off('event', mockFn2);
       Keen.emit('event');
-      expect().equal(count, 1);
+      expect(mockFn1).toBeCalled();
+      expect(mockFn2).not.toBeCalled();
     });
   });
 
   describe('#once', function() {
     it('should call once handlers once when triggered', () => {
-      const countA = 0, countB = 0;
-      function callbackA(){
-        countA++;
-      }
-      function callbackB(){
-        countB++;
-      }
-      Keen.once('event', callbackA);
-      Keen.once('event', callbackB);
+      Keen.once('event', mockFn1);
+      Keen.once('event', mockFn2);
       Keen.emit('event');
-      expect().equal(countA, 1);
-      expect().equal(countB, 1);
+      expect(mockFn1.mock.calls.length).toBe(1);
+      expect(mockFn2.mock.calls.length).toBe(1);
       Keen.emit('event');
-      expect().equal(countA, 1);
-      expect().equal(countB, 1);
+      Keen.emit('event');
+      expect(mockFn1.mock.calls.length).toBe(1);
+      expect(mockFn2.mock.calls.length).toBe(1);
     });
   });
 });
