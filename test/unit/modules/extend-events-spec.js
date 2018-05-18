63701dfd28a8178713cbabd73c02b1422f2ff5d0
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-05-18 16:26:57 +0200

    fix tests for extend events

--- test/unit/modules/extend-events-spec.js
+++ test/unit/modules/extend-events-spec.js
@@ -1,13 +1,15 @@
-
-import Keen from '../../../../lib/server';
+import Keen from '../../../lib/server';
 import config from '../helpers/client-config';
 
-describe('.extendEvent(s) methods', function() {
+describe('.extendEvent(s) methods', () => {
+  let client1;
+  const mockFn1 = jest.fn();
 
-  beforeEach(function() {
+  beforeEach(() => {
+    mockFn1.mockReset();
     Keen.enabled = false;
     Keen.debug = true;
-    this.client = new Keen({
+    client1 = new Keen({
       projectId: config.projectId,
       writeKey: config.writeKey,
       requestType: 'xhr',
@@ -17,79 +19,75 @@ describe('.extendEvent(s) methods', function() {
   });
 
   it('should have an extensions hash with "events" and "collections" queues', () => {
-    expect().isObject(this.client.extensions);
-    expect().isArray(this.client.extensions.events);
-    expect().isObject(this.client.extensions.collections);
+    expect(client1.extensions).toBeInstanceOf(Object);
+    expect(client1.extensions.events).toBeInstanceOf(Array);
+    expect(client1.extensions.collections).toBeInstanceOf(Object);
   });
 
   it('should store global extensions in the proper order', () => {
-    this.client.extendEvents({ key: 123 });
-    this.client.extendEvents(() => { return {} });
-    this.client.extendEvents({ key: 456 });
+    client1.extendEvents({ key: 123 });
+    client1.extendEvents(() => { return {} });
+    client1.extendEvents({ key: 456 });
 
-    expect().isObject(this.client.extensions.events[0]);
-    expect().equal(this.client.extensions.events[0].key, 123);
-    expect().isFunction(this.client.extensions.events[1]);
-    expect().isObject(this.client.extensions.events[2]);
-    expect().equal(this.client.extensions.events[2].key, 456);
+    expect(client1.extensions.events[0]).toBeInstanceOf(Object);
+    expect(client1.extensions.events[0].key).toBe(123);
+    expect(client1.extensions.events[1]).toBeInstanceOf(Function);
+    expect(client1.extensions.events[2]).toBeInstanceOf(Object);
+    expect(client1.extensions.events[2].key).toBe(456);
   });
 
   it('should store single collection extensions in the proper order', () => {
-    this.client.extendEvent('pageviews', { key: 123 });
-    this.client.extendEvent('pageviews', () => { return {} });
-    this.client.extendEvent('pageviews', { key: 456 });
+    client1.extendEvent('pageviews', { key: 123 });
+    client1.extendEvent('pageviews', () => { return {} });
+    client1.extendEvent('pageviews', { key: 456 });
 
-    this.client.extendEvent('purchases', { key: 123 });
-    this.client.extendEvent('purchases', () => { return {} });
-    this.client.extendEvent('purchases', { key: 456 });
+    client1.extendEvent('purchases', { key: 123 });
+    client1.extendEvent('purchases', () => { return {} });
+    client1.extendEvent('purchases', { key: 456 });
 
-    expect().isObject(this.client.extensions.collections['pageviews'][0]);
-    expect().equal(this.client.extensions.collections['pageviews'][0].key, 123);
-    expect().isFunction(this.client.extensions.collections['pageviews'][1]);
-    expect().isObject(this.client.extensions.collections['pageviews'][2]);
-    expect().equal(this.client.extensions.collections['pageviews'][2].key, 456);
+    expect(client1.extensions.collections['pageviews'][0]).toBeInstanceOf(Object);
+    expect(client1.extensions.collections['pageviews'][0].key).toBe(123);
+    expect(client1.extensions.collections['pageviews'][1]).toBeInstanceOf(Function);
+    expect(client1.extensions.collections['pageviews'][2]).toBeInstanceOf(Object);
+    expect(client1.extensions.collections['pageviews'][2].key).toBe(456);
 
-    expect().isObject(this.client.extensions.collections['purchases'][0]);
-    expect().equal(this.client.extensions.collections['purchases'][0].key, 123);
-    expect().isFunction(this.client.extensions.collections['purchases'][1]);
-    expect().isObject(this.client.extensions.collections['purchases'][2]);
-    expect().equal(this.client.extensions.collections['purchases'][2].key, 456);
+    expect(client1.extensions.collections['purchases'][0]).toBeInstanceOf(Object);
+    expect(client1.extensions.collections['purchases'][0].key).toBe(123);
+    expect(client1.extensions.collections['purchases'][1]).toBeInstanceOf(Function);
+    expect(client1.extensions.collections['purchases'][2]).toBeInstanceOf(Object);
+    expect(client1.extensions.collections['purchases'][2].key).toBe(456);
   });
 
   it('should emit an "extendEvent" event', () => {
-    this.client.on('extendEvent', function(eventCollection, eventModifier){
-      expect().equal(eventCollection, 'pageviews');
-      expect().equal(eventModifier.key, 123);
-    });
-    this.client.extendEvent('pageviews', { key: 123 });
+    client1.on('extendEvent', mockFn1);
+    client1.extendEvent('pageviews', { key: 123 });
+    expect(mockFn1).toBeCalledWith('pageviews', { key: 123 });
   });
 
   it('should emit an "extendEvents" event', () => {
-    this.client.on('extendEvents', function(eventsModifier){
-      expect().equal(eventsModifier.key, 123);
-    });
-    this.client.extendEvents({ key: 123 });
+    client1.on('extendEvents', mockFn1);
+    client1.extendEvents({ key: 123 });
+    expect(mockFn1).toBeCalledWith({ key: 123 });
   });
 
 
   describe('when calling .recordEvent', () => {
 
     it('should extend the event body of all events', () => {
-      this.client.on('recordEvent', function(eventCollection, eventBody){
-        expect().equal(eventCollection, 'test');
-        expect().deepEqual(eventBody, { key: 123, exists: true, user: { id: '3434', active: false } });
-      });
-      this.client.extendEvents({ key: 123, user: { id: '3434' } });
-      this.client.recordEvent('test', { exists: true, user: { active: false } });
+      client1.on('recordEvent', mockFn1);
+      client1.extendEvents({ key: 123, user: { id: '3434' } });
+      client1.recordEvent('test', { exists: true, user: { active: false } });
+      expect(mockFn1).toBeCalledWith('test', { key: 123, exists: true, user: { id: '3434', active: false }});
     });
 
     it('should extend the event body of a specific collection', () => {
-      this.client.on('recordEvent', function(eventCollection, eventBody){
-        expect().equal(eventCollection, 'test');
-        expect().deepEqual(eventBody, { key: 123, exists: true, user: { id: '3434', active: false } });
-      });
-      this.client.extendEvent('test', { key: 123, user: { id: '3434' } });
-      this.client.recordEvent('test', { exists: true, user: { active: false } });
+      client1.on('recordEvent', mockFn1);
+      client1.extendEvent('test', { key: 123, user: { id: '3434' } });
+      client1.recordEvent('test', { exists: true, user: { active: false } });
+      expect(mockFn1).toBeCalledWith('test', { key: 123, exists: true, user: { id: '3434', active: false }});
+      mockFn1.mockReset();
+      client1.recordEvent('test2', { exists: true, user: { active: false } });
+      expect(mockFn1).not.toBeCalledWith('test', { key: 123, exists: true, user: { id: '3434', active: false }});
     });
 
   });
@@ -97,25 +95,24 @@ describe('.extendEvent(s) methods', function() {
   describe('when calling .recordEvents', () => {
 
     it('should extend the event body of all events', () => {
-      this.client.on('recordEvents', function(eventsHash){
-        expect().deepEqual(eventsHash, {
-          'test 1': [
-            { key: 123, exists: true, user: { id: '424234', key: 0 } }
-          ],
-          'test 2': [
-            { key: 123, exists: false, test: 'string', user: { id: '424234' } }
-          ],
-          'test 3': [
-            { key: 456, user: { id: '424234' } },
-            { key: 123, exists: true, user: { id: '424234' } }
-          ]
-        });
-      });
-      this.client.extendEvents({ key: 123, user: { id: '424234' } });
-      this.client.recordEvents({
+      client1.on('recordEvents', mockFn1);
+      client1.extendEvents({ key: 123, user: { id: '424234' } });
+      client1.recordEvents({
         'test 1': [ { exists: true, user: { key: 0 } } ],
         'test 2': [ { exists: false, test: 'string' } ],
         'test 3': [ { key: 456 }, { exists: true } ]
+      });
+      expect(mockFn1).toBeCalledWith({
+        'test 1': [
+          { key: 123, exists: true, user: { id: '424234', key: 0 } }
+        ],
+        'test 2': [
+          { key: 123, exists: false, test: 'string', user: { id: '424234' } }
+        ],
+        'test 3': [
+          { key: 456, user: { id: '424234' } },
+          { key: 123, exists: true, user: { id: '424234' } }
+        ]
       });
     });
 
