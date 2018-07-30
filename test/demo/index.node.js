65239df7a2777580126c2f2f21d533774926d8f4
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-07-30 10:55:19 +0200

    faster configuration for dev envs - custom builds of the lib

--- test/demo/index.node.js
+++ test/demo/index.node.js
@@ -1,6 +1,5 @@
 // NODE.js tests
-const demoConfig = require('./demo-config');
 const demoKeen = require('../../');
 const demoTests = require('./demo-tests');
 
-demoTests(demoConfig, demoKeen);
+demoTests(demoKeen.keenGlobals.demoConfig, demoKeen);
