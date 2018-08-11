2bafd64c2b2d82ca3e3c017a36b9f3a5c4e94de5
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-08-11 18:11:58 +0200

    extend tests for autotracking

--- test/setupJest.js
+++ test/setupJest.js
@@ -1,4 +1,6 @@
 global.fetch = require('jest-fetch-mock');
-
+global.navigator = {
+  sendBeacon: jest.mock()
+};
 jest.mock('promise-polyfill', () => {});
 jest.mock('promise-polyfill/src/polyfill', () => {});
